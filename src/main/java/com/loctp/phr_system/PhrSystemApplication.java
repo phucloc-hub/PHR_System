8afcd524dab7027b7f1298961579445e14f20a93
LocTP <trphucloc@gmail.com>
2021-06-03 08:15:21 +0700

    first commit to locTP branch

---
+++ src/main/java/com/loctp/phr_system/PhrSystemApplication.java
@@ -0,0 +1,13 @@
+package com.loctp.phr_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhrSystemApplication.class, args);
+    }
+
+}
