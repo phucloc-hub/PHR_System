b728ed44c040abf84d26c70551c5a66eec5cf7c4
LocTP <trphucloc@gmail.com>
2021-06-11 09:44:20 +0700

    commit func Create Clinic's Group

---
+++ src/main/java/com/loctp/phr_system/service/ClinicGroupService.java
@@ -0,0 +1,37 @@
+package com.loctp.phr_system.service;
+
+import com.loctp.phr_system.dto.ClinicGroupDTO;
+import com.loctp.phr_system.model.ClinicGroup;
+import com.loctp.phr_system.repository.IClinicGroupRepository;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ClinicGroupService implements IClinicGroupService{
+
+
+
+    Logger logger = LoggerFactory.getLogger(ClinicGroupService.class);
+
+    @Autowired
+    private IClinicGroupRepository repository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public ClinicGroupDTO createClinicGroup(ClinicGroupDTO dto) {
+        if(repository.countByNameIgnoreCase(dto.getName()) == 0){
+            ClinicGroup clinicGroup = mapper.map(dto,ClinicGroup.class);
+            clinicGroup = repository.save(clinicGroup);
+            return mapper.map(clinicGroup,ClinicGroupDTO.class);
+        }
+        return dto;
+
+    }
+
+}
--- src/main/java/com/loctp/phr_system/service/IClinicGroupService.java
+++ src/main/java/com/loctp/phr_system/service/IClinicGroupService.java
@@ -1,4 +1,7 @@
 package com.loctp.phr_system.service;
 
+import com.loctp.phr_system.dto.ClinicGroupDTO;
+
 public interface IClinicGroupService{
+    ClinicGroupDTO createClinicGroup(ClinicGroupDTO dto);
 }
