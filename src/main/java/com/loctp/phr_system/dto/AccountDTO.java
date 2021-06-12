11ae9874a3020f1195d06e21d319c7d1682220f2
LocTP <trphucloc@gmail.com>
2021-06-12 09:28:53 +0700

    commit func Create Account and Delete Account LocTP

--- src/main/java/com/loctp/phr_system/dto/AccountDTO.java
+++ src/main/java/com/loctp/phr_system/dto/AccountDTO.java
@@ -3,14 +3,21 @@ package com.loctp.phr_system.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @Getter
 @Setter
 public class AccountDTO {
 
     private Integer id;
 
+    @NotNull
+    @NotEmpty
     private String password;
 
+    @NotNull
+    @NotEmpty
+    @Size(max = 10)
     private String phone;
 
     private String roleId;
