4d6b196fd3f974fa9eb91513b88932fb18b219a0
LocTP <trphucloc@gmail.com>
2021-06-12 08:51:56 +0700

    commit func GetAll and Get Clinic Details by ID LocTP

--- src/main/java/com/loctp/phr_system/service/IClinicService.java
+++ src/main/java/com/loctp/phr_system/service/IClinicService.java
@@ -3,6 +3,8 @@ package com.loctp.phr_system.service;
 
 import com.loctp.phr_system.dto.ClinicDTO;
 
+import java.util.List;
+
 public interface IClinicService  {
     ClinicDTO getClinicById(Integer id);
     Boolean checkClinicAvailable(Integer id);
@@ -10,5 +12,6 @@ public interface IClinicService  {
     ClinicDTO createClinic(ClinicDTO dto);
     Boolean updateClinic(ClinicDTO dto);
     Boolean disableClinicById(Integer id);
+    List<ClinicDTO> getAll();
 
 }
