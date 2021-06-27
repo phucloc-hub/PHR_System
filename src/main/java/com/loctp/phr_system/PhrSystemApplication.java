24e19f95cf15637924fb80f45fac16fa44a5306a
hungnvse <hunguyen1499@gmail.com>
2021-06-28 01:37:01 +0700

    build war successfully

--- src/main/java/com/loctp/phr_system/PhrSystemApplication.java
+++ src/main/java/com/loctp/phr_system/PhrSystemApplication.java
@@ -3,6 +3,7 @@ package com.loctp.phr_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EntityScan("com.loctp.phr_system.model")
 @EnableJpaRepositories("com.loctp.phr_system.repository")
 @EnableSwagger2
-public class PhrSystemApplication {
+public class PhrSystemApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PhrSystemApplication.class, args);
